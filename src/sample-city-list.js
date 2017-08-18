0d81ab76477e0bc021e9541fdfe2f5587e6e2140
Euan_G <euan@amwic.co.za>
2017-08-18 22:13:32 +0200

    Add more sample data

---
+++ src/sample-city-list.js
@@ -0,0 +1,201 @@
+// This is just some sample data so you don't have to think of your own!
+module.exports = {
+  Alberton: {
+    Suburb: ["Alberton"]
+  },
+
+  CapeTown: {
+    Suburb: [
+      "Acacia Park",
+      "Athlone",
+      "Bellville",
+      "Blouberg",
+      "Bo Kaap",
+      "Camps Bay",
+      "CBD",
+      "Century City Ext",
+      "Claremont",
+      "Constantia",
+      "De Waterkant",
+      "Durbanville",
+      "Foreshore",
+      "Gardens",
+      "Green Point",
+      "Harfield Village",
+      "Hout Bay",
+      "Kenilworth",
+      "Kraaifontein",
+      "Meadowridge",
+      "Milnerton",
+      "Mowbray",
+      "Newlands CPT",
+      "Observatory",
+      "Panorama",
+      "Parklands",
+      "Parow",
+      "Pinelands",
+      "Plattekloof 1",
+      "Rondebosch",
+      "Salt River",
+      "Sea Point",
+      "Steenberg",
+      "Tamboerskloof",
+      "Tokai",
+      "Tyger Waterfront",
+      "Tygervalley Ext",
+      "Vredehoek",
+      "Vredekloof",
+      "Waterfront",
+      "Westlake",
+      "Westlake Business Park",
+      "Woodstock"
+    ]
+  },
+
+  Centurion: {
+    Suburb: [
+      "Centurion Central",
+      "Clubview",
+      "Die Hoewes",
+      "Doringkloof",
+      "Hennopspark",
+      "Lyttelton Manor"
+    ]
+  },
+
+  Durban: {
+    Suburb: [
+      "Durban Central",
+      "Durban North",
+      "Glenashley",
+      "Glenwood",
+      "Morningside",
+      "New Germany",
+      "Stamford Hill",
+      "Umhlanga Ext",
+      "Umhlanga Ridge",
+      "Umhlanga Rocks",
+      "Westville"
+    ]
+  },
+
+  Franschhoek: {
+    Suburb: ["Franschhoek"]
+  },
+
+  Johannesburg: {
+    Suburb: [
+      "Atholl",
+      "Barbeque Downs",
+      "Beaulieu",
+      "Bedford Gardens",
+      "Bedfordview",
+      "Benmore Gardens",
+      "Blairgowrie",
+      "Braamfontein",
+      "Bramley",
+      "Broadacres",
+      "Bryanston",
+      "Craighall",
+      "Cresta",
+      "Douglasdale",
+      "Dunkeld",
+      "Edenvale",
+      "Emmarentia",
+      "Ferndale",
+      "Ferreiras Dorp",
+      "Florida Glen",
+      "Fordsburg",
+      "Fourways",
+      "Gallo Manor",
+      "Glenhazel",
+      "Greenside",
+      "Hyde Park",
+      "Illovo",
+      "JHB CBD Ext",
+      "Johannesburg CBD",
+      "Killarney",
+      "Krugersdorp",
+      "Kyalami Estate",
+      "Kyalami Gardens",
+      "Kyalami Park",
+      "Linden",
+      "Lonehill",
+      "Marshalltown",
+      "Melrose",
+      "Melville",
+      "Morningside",
+      "Morningside Manor",
+      "New Town",
+      "Northcliff",
+      "Norwood",
+      "Parkhurst",
+      "Parkmore",
+      "Parktown",
+      "Parktown North",
+      "Parkview",
+      "Parkwood",
+      "Paulshof",
+      "Randburg",
+      "Randpark Ridge",
+      "Riverclub",
+      "Rivonia",
+      "Roodepoort",
+      "Rosebank JHB",
+      "Sandown",
+      "Sandton",
+      "Savoy",
+      "Strubens Valley",
+      "Sunninghill",
+      "Sunnyside",
+      "Waterfall",
+      "Waterfall Estate",
+      "Weltevredenpark",
+      "Woodmead",
+      "Wynberg"
+    ]
+  },
+
+  KemptonPark: {
+    Suburb: ["Kempton Park"]
+  },
+
+  Paarl: {
+    Suburb: ["Paarl"]
+  },
+
+  Pretoria: {
+    Suburb: [
+      "Brooklyn PTA",
+      "Constantia Park",
+      "Die Wilgers",
+      "Equestria",
+      "Faerie Glen",
+      "Garsfontein",
+      "Garsfontein Ext",
+      "Hatfield",
+      "Hazeldean",
+      "Hazelwood",
+      "Lynnwood",
+      "Lynnwood Manor",
+      "Lynnwood Ridge",
+      "Menlo Park",
+      "Menlyn",
+      "Moreletapark",
+      "Newlands PTA",
+      "Waterkloof Heights"
+    ]
+  },
+
+  SomersetWest: {
+    Suburb: ["Somerset West"]
+  },
+
+  Stellenbosch: {
+    Suburb: ["Stellenbosch Central", "Stellenbosch Ext"]
+  },
+
+  Strand: {
+    Suburb: ["Strand"]
+  }
+};
