60bf31925ee174a8850607192bc17ba6fd5adbb7
Euan_G <euan@amwic.co.za>
2017-08-25 15:04:54 +0200

    Removed all whitespace for url path

--- src/components/SearchResults/Store.js
+++ src/components/SearchResults/Store.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Store = ({ details }) =>
-  <li className="media">
-    <div className="media-left">
-      <img src={details.image} alt="logo" className="image is-64x64" />
-    </div>
-    <div className="media-content">
-      <div className="content">
-        <h5 className="title">
-          {details.name}
-        </h5>
-        <p className="subtitle">
-          {details.address}
-        </p>
+
+const Store = ({ details }) => {
+  const storeId = details.name.replace(/\s+/g, "");
+  return (
+    <li className="media">
+      <div className="media-left">
+        <img src={details.image} alt="logo" className="image is-64x64" />
       </div>
-    </div>
-    <div className="media-right">
-      <Link to={`/order-online/${details.name}`}>
-        <button className="button is-danger is-outlined"> Order Now </button>
-      </Link>
-    </div>
-  </li>;
+      <div className="media-content">
+        <div className="content">
+          <h5 className="title">
+            {details.name}
+          </h5>
+          <p className="subtitle">
+            {details.address}
+          </p>
+        </div>
+      </div>
+      <div className="media-right">
+        <Link to={`/order-online/${storeId}`}>
+          <button className="button is-danger is-outlined"> Order Now </button>
+        </Link>
+      </div>
+    </li>
+  );
+};
 
 export default Store;
