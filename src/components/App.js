720640badfeeb33f6a83ced081f838e45266d24f
Euan_G <euan@amwic.co.za>
2017-08-28 11:25:34 +0200

    Add Home Component and Change Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,18 @@
-import React, { Component } from "react";
-import SearchBox from "./Search/SearchBox";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
 import SearchByCuisine from "./Search/SearchByCuisine/SearchByCuisine";
 import SearchByCity from "./Search/SearchByCity/SearchByCity";
-import SearchResultsList from "./SearchResults/SearchResultsList";
+import NotFound from "./NotFound";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchResult: 0
-    };
-  }
-
-  fromSearchForm(deliveryAddress) {
-    // console.log(deliveryAddress);
-    // this.setState({ deliveryAddress });
-  }
-
-  searchSelected(suburb, city, cuisine) {
-    const searchItem = {
-      suburb,
-      city,
-      cuisine
-    };
-    this.setState({ searchResult: searchItem });
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBox
-          searchSelected={this.searchSelected.bind(this)}
-          fromSearchForm={this.fromSearchForm.bind(this)}
-        />
-        <SearchByCity searchSelected={this.searchSelected.bind(this)} />
-        <SearchByCuisine searchSelected={this.searchSelected.bind(this)} />
-        {this.state.searchResult !== 0
-          ? <SearchResultsList searchResult={this.state.searchResult} />
-          : <div />}
-      </div>
-    );
-  }
-}
+const App = () =>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/Cities/:cityId" component={SearchByCity} />
+      <Route path="/Cuisine/:cuisineId" component={SearchByCuisine} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
 import "font-awesome/css/font-awesome.css";
 import App from "./components/App";
-import NotFound from "./components/NotFound";
 import registerServiceWorker from "./registerServiceWorker";
 
-const Root = () =>
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={App} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>;
-
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
