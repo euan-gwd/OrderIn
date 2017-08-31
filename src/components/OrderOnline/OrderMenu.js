42f7da5538a05d47d16049dd77476a8b004fd009
Euan_G <euan@amwic.co.za>
2017-08-31 16:59:56 +0200

    initial commit on branch

---
+++ src/components/OrderOnline/OrderMenu.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class OrderMenu extends Component {
+  state = {};
+  render() {
+    return <div className="">test</div>;
+  }
+}
+
+export default OrderMenu;
