34d001fe4fb6db278e0ea2edbcd30bfee6bb6c7a
Euan_G <euan@amwic.co.za>
2017-09-01 12:35:28 +0200

    Cleanup Folder Structure

--- src/components/OrderOnline/OrderOnline.js
+++ src/components/OrderOnline/OrderOnline.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./orderonline_styles.css";
-import OrderBreadCrumbNav from "./OrderBreadCrumbNav";
-import OrderMenu from "./OrderMenu";
+import OrderBreadCrumbNav from "./OrderNav/OrderBreadCrumbNav";
+import OrderMenu from "./OrderMenu/OrderMenu";
 
 const OrderOnline = ({ name, restaurantsData }) => (
   <div className="StoreMenu">
