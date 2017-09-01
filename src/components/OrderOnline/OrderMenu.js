4c19aad24fc00b1b26311ab23fc1645018c4a562
Euan_G <euan@amwic.co.za>
2017-09-01 12:17:52 +0200

    Add Styling to Order Containers

--- src/components/OrderOnline/OrderMenu.js
+++ src/components/OrderOnline/OrderMenu.js
@@ -4,13 +4,29 @@ import "./ordermenustyles.css";
 class OrderMenu extends Component {
   render() {
     const { name, restaurantsData } = this.props;
-    // const restaurant = restaurantsData.find(restaurant => restaurant.name === name);
-    console.log(name);
-    console.log(restaurantsData);
-    // console.log(restaurant);
+    const restaurant = restaurantsData.find(restaurant => restaurant.name === name);
     return (
       <div className="store-menu">
-        <header className="store-header">Store info</header>
+        <header className="store-header">
+          <div className="media">
+            <div className="media-left">
+              <img src={restaurant.image} alt="logo" className="image is-64x64" />
+            </div>
+            <div className="media-content">
+              <div className="">
+                <h3 className="is-size-4 has-text-grey-dark">{restaurant.name}</h3>
+                <span className="has-text-grey-light">
+                  <i className="fa fa-icon fa-map-marker" />
+                  {restaurant.address}
+                </span>
+                <span className="has-text-grey-light">
+                  <i className="fa icon-padding fa-phone" />
+                  {restaurant.tel}
+                </span>
+              </div>
+            </div>
+          </div>
+        </header>
         <main className="store-main-content">Menu</main>
         <aside className="store-sidebar">Order</aside>
       </div>
--- src/mock/sample-stores.js
+++ src/mock/sample-stores.js
@@ -3,54 +3,63 @@ export const Stores = [
   {
     name: "2 Kitchens",
     image: require("./images/Logo 2Kitchens.png"),
-    address: "8 Stowe Street"
+    address: "8 Stowe Street",
+    tel: "000-000-000"
   },
 
   {
     name: "Bingo",
     image: require("./images/Bingo Logo.jpg"),
-    address: "5 Sir Lowry Road"
+    address: "5 Sir Lowry Road",
+    tel: "000-000-000"
   },
 
   {
     name: "Cyber Cellar",
     image: require("./images/Cybercellar.jpg"),
-    address: "175 Victoria Road"
+    address: "175 Victoria Road",
+    tel: "000-000-000"
   },
 
   {
     name: "Fat Cactus",
     image: require("./images/fat cactus final logo.jpg"),
-    address: "160 Albert Road"
+    address: "160 Albert Road",
+    tel: "000-000-000"
   },
 
   {
     name: "Fishaways",
     image: require("./images/fishaways_tag.png"),
-    address: "169 Victoria Road"
+    address: "169 Victoria Road",
+    tel: "000-000-000"
   },
 
   {
     name: "Truck Food Mafia",
     image: require("./images/food truck mafia logo new.png"),
-    address: "19 Kent Street"
+    address: "19 Kent Street",
+    tel: "000-000-000"
   },
 
   {
     name: "Jamaica Me Crazy",
     image: require("./images/Jamaica Me Crazy.jpg"),
-    address: "74 Roodebloem Road"
+    address: "74 Roodebloem Road",
+    tel: "000-000-000"
   },
 
   {
     name: "Three Feathers Diner",
     image: require("./images/Three Feathers Diner.jpg"),
-    address: "86 Bromwell Street"
+    address: "86 Bromwell Street",
+    tel: "000-000-000"
   },
 
   {
     name: "Yum",
     image: require("./images/yum logo.png"),
-    address: "31 Brickfield Road"
+    address: "31 Brickfield Road",
+    tel: "000-000-000"
   }
 ];
