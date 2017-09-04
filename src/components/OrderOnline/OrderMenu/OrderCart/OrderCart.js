f945547b400b9e943f26c1f1b5570fc868de319a
Euan_G <euan@amwic.co.za>
2017-09-04 12:46:58 +0200

    Add Default Styling to OrderCart

--- src/components/OrderOnline/OrderMenu/OrderCart/OrderCart.js
+++ src/components/OrderOnline/OrderMenu/OrderCart/OrderCart.js
@@ -1,8 +1,40 @@
 import React from "react";
+import "./OrderCartStyles.css";
 
 class OrderCart extends React.PureComponent {
   render() {
-    return <div className="store-sidebar">Your Order</div>;
+    return (
+      <div className="store-sidebar">
+        <div className="cart-divider is-size-4">My Order</div>
+        <div className="cart-orderNo">
+          <span>Order Number:</span>
+          <span>#xxxxxx</span>
+        </div>
+        <div className="cart-divider has-text-grey-lighter">
+          <span>No items added yet.</span>
+          <i className="fa icon-padding fa-frown-o" aria-hidden="true" />
+        </div>
+        <div className="cart-totals-wrapper">
+          <div className="cart-totals-line">
+            <span className="">Subtotal:</span>
+            <span>R0.00</span>
+          </div>
+          <div className="cart-totals-line">
+            <span className="">Add Gratuity</span>
+            <input type="number" className="" />
+          </div>
+          <div className="cart-totals-line">
+            <span className="">Total:</span>
+            <span>R0.00</span>
+          </div>
+        </div>
+        <div className="cartitem-spacing">
+          <button type="submit" className="button is-warning is-fullwidth is-medium">
+            Check Out
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
