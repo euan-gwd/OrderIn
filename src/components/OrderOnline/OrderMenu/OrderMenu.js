b13417068fc27631a3e7f6dadbdbd926f53a4a79
Euan_G <euan@amwic.co.za>
2017-09-05 07:58:04 +0200

    inital commit

--- src/components/OrderOnline/OrderMenu/OrderMenu.js
+++ src/components/OrderOnline/OrderMenu/OrderMenu.js
@@ -39,21 +39,23 @@ class OrderMenu extends React.PureComponent {
     const restaurant = restaurantsData.find(restaurant => restaurant.name === restaurantName);
     const menuData = platters;
     return (
-      <div className="store-menu">
-        <StoreInfo restaurant={restaurant} selectDeliveryOption={this.selectDeliveryOption} />
-        <div className="store-main-content">
-          <header className="menu-list-header">
-            <span className="is-size-4">Menu</span>
-            <span>
-              <i className="fa fa-icon fa-leaf has-text-success" />
-              Vegetarian
-            </span>
-          </header>
-          <ul>
-            {menuData.map(menuItem => <OrderMenuItem key={menuItem.name} menuItem={menuItem} />)}
-          </ul>
+      <div className="StoreMenu">
+        <div className="store-menu">
+          <StoreInfo restaurant={restaurant} selectDeliveryOption={this.selectDeliveryOption} />
+          <div className="store-main-content">
+            <header className="menu-list-header">
+              <span className="is-size-4">Menu</span>
+              <span>
+                <i className="fa fa-icon fa-leaf has-text-success" />
+                Vegetarian
+              </span>
+            </header>
+            <ul>
+              {menuData.map(menuItem => <OrderMenuItem key={menuItem.name} menuItem={menuItem} />)}
+            </ul>
+          </div>
+          <OrderCart restaurantName={restaurantName} orderOption={orderOption} />
         </div>
-        <OrderCart restaurantName={restaurantName} orderOption={orderOption} />
       </div>
     );
   }
