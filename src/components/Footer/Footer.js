edfbeb0fcd067dd14a9375df930caec797cab1d9
Euan_G <euan@amwic.co.za>
2017-09-06 20:06:14 +0200

    Clean up Code on footer,header and searchbox

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,19 +21,25 @@ const Footer = () => {
             </a>
           </div>
           <div className="invite-wrapper">
-            <div className="has-text-centered bordered">
-              <p className="has-text-bold">Sign Up your Company</p>
-              <p>With a corporate account, employees can order food quickly and easily.</p>
+            <div className="bordered">
+              <p className="has-text-bold has-text-centered">Sign Up your Company</p>
+              <p className="has-text-grey has-text-centered">
+                With a corporate account, employees can order food quickly and easily.
+              </p>
               <button className="button is-danger">&#9656; Sign Up</button>
             </div>
-            <div className="has-text-centered bordered">
-              <p className="has-text-bold">Add your restaurant to OrderIn!</p>
-              <p>Sign up with South Africa’s Premier Online food ordering service.</p>
+            <div className="bordered">
+              <p className="has-text-bold has-text-centered">Add your restaurant to OrderIn!</p>
+              <p className="has-text-grey has-text-centered">
+                Sign up with South Africa’s Premier Online food ordering service.
+              </p>
               <button className="button is-danger">&#9656; Sign Up</button>
             </div>
-            <div className="has-text-centered">
-              <p className="has-text-bold">Join Our Team</p>
-              <p>We are looking for dynamic individuals to be part of our growth.</p>
+            <div>
+              <p className="has-text-bold has-text-centered">Join Our Team</p>
+              <p className="has-text-grey has-text-centered">
+                We are looking for dynamic individuals to be part of our growth.
+              </p>
               <button className="button is-danger">&#9656; View Jobs</button>
             </div>
           </div>
