97a2d6d86af01ebf5072676b341de7bacf437889
Euan_G <euan@amwic.co.za>
2017-09-09 10:28:15 +0200

    Fix SearchResultsList

--- src/components/Search/SearchResults/SearchResultsList.js
+++ src/components/Search/SearchResults/SearchResultsList.js
@@ -8,23 +8,21 @@ class SearchResultsList extends React.Component {
     const { searchResult, selectStore } = this.props;
     const restaurants = Stores;
     return (
-      <div className="">
-        <div className="">
-          <h2 className="has-text-centered has-text-white is-size-5-touch is-size-3-desktop">
-            {searchResult.cuisineName} Restaurants in {searchResult.suburbName}{" "}
-            {searchResult.cityName}
-          </h2>
-          <div className="storelist-box">
-            {restaurants.map(restaurant => (
-              <Store key={restaurant.name} details={restaurant} selectStore={selectStore} />
-            ))}
-          </div>
-          <footer className="has-text-centered spacer has-text-white">
-            <p className="icon is-large">
-              <i className="fa fa-arrow-up" aria-hidden="true" />
-            </p>
-          </footer>
+      <div className="search-body">
+        <h2 className="has-text-centered has-text-white is-size-5-touch is-size-3-desktop">
+          {searchResult.cuisineName} Restaurants in {searchResult.suburbName}{" "}
+          {searchResult.cityName}
+        </h2>
+        <div className="storelist-box">
+          {restaurants.map(restaurant => (
+            <Store key={restaurant.name} details={restaurant} selectStore={selectStore} />
+          ))}
         </div>
+        <footer className="has-text-centered spacer has-text-white">
+          <p className="icon is-large">
+            <i className="fa fa-arrow-up" aria-hidden="true" />
+          </p>
+        </footer>
       </div>
     );
   }
