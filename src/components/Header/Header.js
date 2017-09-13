4a16bfd16dfb41b90cbfec1401da59bef367a01e
Euan_G <euan@amwic.co.za>
2017-09-13 11:12:50 +0200

    fix icon spacing bug on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => (
       </NavLink>
     </div>
 
-    <div className="headerbar-dropdown dropdown is-hoverable is-hidden-desktop">
+    <div className="headerbar-dropdown dropdown is-hoverable is-right is-hidden-desktop">
       <div className="dropdown-trigger">
         <button className="header-button" aria-haspopup="true" aria-controls="dropdown-menu-user">
           <i className="fa fa-user-circle-o fa-lg" />
@@ -20,26 +20,20 @@ const Header = () => (
       <div className="dropdown-menu" id="dropdown-menu-user" role="menu">
         <div className="header-dropdown-content">
           <NavLink to="/register" className="dropdown-item">
-            <i className="fa fa-icon fa-pencil-square-o" />
+            <i className="fa header-icon fa-pencil-square-o" />
             <span>Register</span>
           </NavLink>
           <NavLink to="/signin" className="dropdown-item">
-            <i className="fa fa-icon fa-lock" />
+            <i className="fa header-icon fa-lock" />
             <span>Login</span>
           </NavLink>
         </div>
       </div>
     </div>
 
-    <div className="headerbar-cart is-hidden-desktop">
-      <NavLink to="/cart" className="button is-danger">
-        <i className="fa fa-icon fa-shopping-cart fa-lg" />
-      </NavLink>
-    </div>
-
     <div className="headerbar-buttons is-hidden-touch">
       <NavLink to="/register" className="button is-danger is-inverted">
-        <i className="fa fa-icon fa-pencil-square-o" />
+        <i className="fa header-icon fa-pencil-square-o" />
         <span>Register</span>
       </NavLink>
       <NavLink to="/signin" className="button is-danger is-inverted">
