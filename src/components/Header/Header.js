7ae070828f54648d086a79f597d394830a8d7f5c
Euan_G <euan@amwic.co.za>
2017-09-13 10:23:37 +0200

    refactored Header for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,34 @@ const Header = () => (
         <img src={Logo} alt="OrderIn Logo" width="112" height="28" />
       </NavLink>
     </div>
-    <div className="headerbar-end">
+
+    <div className="headerbar-dropdown dropdown is-hoverable is-hidden-desktop">
+      <div className="dropdown-trigger">
+        <button className="header-button" aria-haspopup="true" aria-controls="dropdown-menu-user">
+          <i className="fa fa-user-circle-o fa-lg" />
+        </button>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu-user" role="menu">
+        <div className="header-dropdown-content">
+          <NavLink to="/register" className="dropdown-item">
+            <i className="fa fa-icon fa-pencil-square-o" />
+            <span>Register</span>
+          </NavLink>
+          <NavLink to="/signin" className="dropdown-item">
+            <i className="fa fa-icon fa-lock" />
+            <span>Login</span>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+
+    <div className="headerbar-cart is-hidden-desktop">
+      <NavLink to="/cart" className="button is-danger">
+        <i className="fa fa-icon fa-shopping-cart fa-lg" />
+      </NavLink>
+    </div>
+
+    <div className="headerbar-buttons is-hidden-touch">
       <NavLink to="/register" className="button is-danger is-inverted">
         <i className="fa fa-icon fa-pencil-square-o" />
         <span>Register</span>
