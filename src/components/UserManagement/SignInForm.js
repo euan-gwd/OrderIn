091e397f146ed117077adf7e9b1c73ae51ebdc25
Euan_G <euan@amwic.co.za>
2017-09-18 08:56:11 +0200

    Fix User management forms

--- src/components/UserManagement/SignInForm.js
+++ src/components/UserManagement/SignInForm.js
@@ -4,43 +4,37 @@ import "./formstyles.css";
 class SignInForm extends React.PureComponent {
   render() {
     return (
-      <div className="hero is-danger is-medium">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns is-centered">
-              <div className="column is-half box">
-                <h1 className="form-header">Log in and get ordering.</h1>
-                <div className="form-field">
-                  <p className="control has-icons-left has-icons-right">
-                    <input className="input" type="email" placeholder="Email" />
-                    <span className="icon is-small is-left">
-                      <i className="fa fa-envelope" />
-                    </span>
-                  </p>
-                </div>
-                <div className="form-field">
-                  <p className="control has-icons-left">
-                    <input className="input" type="password" placeholder="Password" />
-                    <span className="icon is-small is-left">
-                      <i className="fa fa-user-secret" />
-                    </span>
-                  </p>
-                </div>
-                <div className="form-field">
-                  <div className="control">
-                    <input type="checkbox" className="terms" id="terms" />
-                    <label htmlFor="terms" className="terms-label">
-                      Remember Me?
-                    </label>
-                  </div>
-                </div>
-                <div className="form-field">
-                  <p className="control">
-                    <button className="button is-danger is-fullwidth is-medium has-text-centered">Sign In</button>
-                  </p>
-                </div>
-              </div>
+      <div className="user-management-container">
+        <div className="user-management-body">
+          <h1 className="form-header">Log in and get ordering.</h1>
+          <div className="form-field">
+            <p className="control has-icons-left has-icons-right">
+              <input className="input" type="email" placeholder="Email" />
+              <span className="icon is-small is-left">
+                <i className="fa fa-envelope" />
+              </span>
+            </p>
+          </div>
+          <div className="form-field">
+            <p className="control has-icons-left">
+              <input className="input" type="password" placeholder="Password" />
+              <span className="icon is-small is-left">
+                <i className="fa fa-user-secret" />
+              </span>
+            </p>
+          </div>
+          <div className="form-field">
+            <div className="control">
+              <input type="checkbox" className="terms" id="terms" />
+              <label htmlFor="terms" className="terms-label">
+                Remember Me?
+              </label>
             </div>
+          </div>
+          <div className="form-field">
+            <p className="control">
+              <button className="user-Button">Sign In</button>
+            </p>
           </div>
         </div>
       </div>
